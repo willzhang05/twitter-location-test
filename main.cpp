2602d7d1213f82e79bb7211ba0acc270f4c4a599
William Zhang <willzhang05@gmail.com>
2016-03-06 23:15:15 -0500

    Fixed some stuff, started working on calling Twitter API

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
+#include <cstdlib>
 #include <fstream>
 #include <string>
 #include <curl/curl.h>
+
+const char RAND_TABLE[63] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 std::string find_and_replace(std::string &s, const std::string &to_replace, const std::string &replace_with) {
 	return s.replace(s.find(to_replace), to_replace.length(), replace_with);
 }
 
+std::string gen_alphanum(int len) {
+	std::string buff;
+	std::srand(time(0));
+	for(int i = 0; i < len; i++) {
+		buff += RAND_TABLE[std::rand() % 62];
+	}
+	return buff;
+}
+
+
 int main () {
 	std::string line;
 	std::string appinfo [4];
-	int type = 0;
 	std::ifstream infile("twitter.conf");
 	while(std::getline(infile, line)) {
 		if(line.find("ckey") != std::string::npos) {
@@ -34,6 +46,14 @@ int main () {
 	std::cout << "Enter Twitter Username: ";
 	getline(std::cin, username);
 	std::cout << "\n";
+	std::string nonce = gen_alphanum(42);
+	printf("%s\n", nonce.c_str());
+	/*
+	std::string command = "curl --get 'https://api.twitter.com/1.1/users/lookup.json' --data 'screen_name=" + username + "' --header 'Authorization: OAuth oauth_consumer_key=\"" + appinfo[0] + "\", oauth_nonce=\"14f3ace6df27f3ea8322ba20e3e509c5\", oauth_signature=\"h18hZk1pMPAc8uaxBNLqvc1fQLU\%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1457318794\", oauth_token=\"" + appinfo[2] + "\", oauth_version=\"1.0\"' --verbose > lookup.json";
+	std::system(command.c_str());
+	*/
+	//std::system("cat lookup.json");
+	/*
 	std::string url = "https://twitter.com/" + username + "#page-container";
 	CURL *curl;
     CURLcode res;
@@ -43,6 +63,6 @@ int main () {
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
-    }
+    }*/
     return 0;
 }
